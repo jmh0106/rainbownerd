9588828114046637bc6467591cfa5a1edf8fb0ab
jmh0106 <50164268+jmh0106@users.noreply.github.com>
2021-10-03 17:53:14 +0900

    캐나다 서머타임
    캐나다 서머타임 관련 함수 변경

--- run.py
+++ run.py
@@ -180,9 +180,9 @@ def showTime(userName, i):
     #시간 가져오기
     KST = datetime.datetime.now() + datetime.timedelta(hours = 9 + i)
     if datetime.datetime.now() < datetime.datetime(2021, 11, 7):
-        PST = datetime.datetime.now() - datetime.timedelta(hours = 8 - i)
-    else:
         PST = datetime.datetime.now() - datetime.timedelta(hours = 7 - i)
+    else:
+        PST = datetime.datetime.now() - datetime.timedelta(hours = 8 - i)
 
     #임베디드 생성
     embed = discord.Embed(title = userName + "님의 시간", description = "====================")
--- test.py
+++ test.py
@@ -1,33 +1,6 @@
-# 파일 임포트
-from oauth2client.service_account import ServiceAccountCredentials
-import gspread
+import datetime
 
-# 권한 불러오기
-scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-credentials = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
-sheetAuth = gspread.authorize(credentials)
-
-# 파일 열기
-sheetNote = sheetAuth.open("RainbowNerdData").worksheet('메모')
-sheetNoteValue = sheetNote.get_all_values()
-
-def printNote():
-    for i in sheetNoteValue:
-        print(i[0] + i[1])
-
-def writeNote(userName, noteDes):
-    sheetNoteValue = sheetNote.get_all_values()
-    writeNum = str(len(sheetNoteValue) + 1)
-    sheetNote.update_acell("A" + writeNum, userName)
-    sheetNote.update_acell("B" + writeNum, noteDes)
-    sheetNoteValue = sheetNote.get_all_values()
-
-def deleteNote(userName, noteNum):
-    sheetNoteValue = sheetNote.get_all_values()
-    if (userName == sheetNoteValue[noteNum - 1][0]):
-        for i in range(noteNum, len(sheetNoteValue)):
-            sheetNote.update("A" + str(i), sheetNoteValue[i][0])
-            sheetNote.update("B" + str(i), sheetNoteValue[i][1])
-        sheetNote.update("A" + str(len(sheetNoteValue)), '')
-        sheetNote.update("B" + str(len(sheetNoteValue)), '')
-        sheetNoteValue = sheetNote.get_all_values()
+if datetime.datetime.now() < datetime.datetime(2021, 11, 7):
+    print("서머타임중")
+else:
+    print("서머타임아님")
